7214682d13791d6af142c777afc5ea68add9137a
gwynnbleiidd <midi2001@mail.ru>
2023-12-10 12:08:05 +0200

    change logger to console

--- devtd.js
+++ devtd.js
@@ -430,7 +430,7 @@ Lampa.Listener.follow("torrent", function (e) {
                                       })
                                     })
                                     .then(response => {
-                                        Lampa.Noty.show(JSON.stringify(response));
+                                        console.log(JSON.stringify(response));
                                       if (response.ok) {
                                         // Параметры для добавления торрента
                                         const addBody = {
