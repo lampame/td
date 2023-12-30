1935156a193d279c1dd366072110b4b3e94fecc8
Felix <midi2001@mail.ru>
2023-12-30 20:34:38 +0200

    Update devtd.js

--- devtd.js
+++ devtd.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	var script = document.createElement('script');
-	script.src = 'https://lampame.github.io/td/td.js';
+	script.src = 'https://lampame.github.io/td/td/td.js';
 	document.head.appendChild(script);
 
 })();
